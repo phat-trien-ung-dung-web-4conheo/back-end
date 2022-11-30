18a38944505f8ea15e03db568ddbd2e2e8f65142
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2022-12-01 01:45:04 +0700

    update code

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.use("/api/orders", orderRoute);
 app.use(cors());
 app.use(express.json());
 
-port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started ${port}`));
