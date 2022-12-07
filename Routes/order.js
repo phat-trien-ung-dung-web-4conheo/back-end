83f88c4dee44563f26ab7e346ebf555e52334c92
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2022-12-07 12:02:16 +0700

    update find all order

--- Routes/order.js
+++ Routes/order.js
@@ -46,7 +46,7 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 //GET USER ORDER
 router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
   try {
-    const order = await Order.findOne({ userId: req.params.userId });
+    const order = await Order.find({ userId: req.params.userId });
     res.status(200).json(order);
   } catch (err) {
     res.status(500).json(err);
